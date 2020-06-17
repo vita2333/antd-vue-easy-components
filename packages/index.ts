be04a4dd4872c89fb0d6af12fc4f19f5b4906179
vita2333 <lp_vitadolce@163.com>
2020-06-17 12:58:29 +0800

    chore: add types

--- packages/index.ts
+++ packages/index.ts
@@ -1,15 +1,23 @@
 import TestComponent from './test-component'
-import { PluginFunction } from 'vue'
+import DynamicForm from './dynamic-form'
+import { VueConstructor } from 'vue'
 import pkg from '../package.json'
 
-const components = [TestComponent]
-const install: PluginFunction<Vue> = (Vue) => {
+const version = pkg.version
+const components = [TestComponent, DynamicForm]
+const install: any = (Vue: VueConstructor<any>) => {
   components.forEach((component) => {
     Vue.component(component.name, component)
   })
 }
 
-export default {
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export {
   install,
-  version: pkg.version,
+  version,
+  TestComponent,
+  DynamicForm,
 }
---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+import { DynamicForm } from './dynamic-form'
+import { Component } from './component'
+
+export class TestComponent extends Component {
+
+}
+
+export {
+  DynamicForm,
+}
+
+export default Component
